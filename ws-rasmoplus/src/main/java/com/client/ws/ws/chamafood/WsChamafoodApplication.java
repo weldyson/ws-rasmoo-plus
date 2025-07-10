83288d1958c715a4419648678639a0fc9a4eda40
Azevedo <weldysona@gmail.com>
2025-07-09 21:36:35 -0300

    Initial commit

---
+++ ws-rasmoplus/src/main/java/com/client/ws/ws/chamafood/WsChamafoodApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.ws.chamafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsChamafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsChamafoodApplication.class, args);
+	}
+
+}
---
+++ ws-rasmoplus/src/test/java/com/client/ws/ws/chamafood/WsChamafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.ws.ws.chamafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsChamafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
